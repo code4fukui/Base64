233bb62d2ad2e37ab79f4ecfcabd5d0d3870df42
Taisuke Fukuno <fukuno@jig.jp>
2023-01-11 12:44:16 +0900

    improve

--- Base64.js
+++ Base64.js
@@ -1,15 +1,13 @@
 class Base64 {
   static encode(bin) {
     const ss = [];
-    bin.map(b => ss.push(String.fromCharCode(b)));
+    bin.forEach(b => ss.push(String.fromCharCode(b)));
     return btoa(ss.join(""));
   }
   static decode(text) {
     const s = atob(text);
     const res = new Uint8Array(s.length);
-    for (let i = 0; i < s.length; i++) {
-      res[i] = s.charCodeAt(i);
-    }
+    res.forEach((_, i) => res[i] = s.charCodeAt(i));
     return res;
   }
 }
