91a432e4930688a36ce5e8ed656e671d87094a7e
Taisuke Fukuno <fukuno@jig.jp>
2023-01-11 12:44:20 +0900

    fix test

--- Base64.test.js
+++ Base64.test.js
@@ -16,10 +16,11 @@ Deno.test("decode", () => {
   const s = "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq+wsbKztLW2t7i5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+/w==";
   const bin = Base64.decode(s);
   //console.log("bin", bin.length);
-  //t.assertEquals(bin, test);
+  t.assertEquals(bin, test);
 });
 Deno.test("performance", () => {
-  const size = 1024 * 1024;
+  const size = 1024 * 1024; // 600msec
+  //const size = 1024 * 1024 * 10; // 10sec
   // const size = 1024 * 1024 * 64;
   const test = new Uint8Array(size);
   t.assertEquals(Base64.decode(Base64.encode(test)), test);
